bd8052d175fb63ae58e6c0551a0aaf714911a6bd
270559419@qq.com <270559419@qq.com>
2018-05-17 09:57:18 +0800

    1

---
+++ MyApplication/mylibrary/src/main/java/com/zhangy/mylibrary/Test.java
@@ -0,0 +1,13 @@
+package com.zhangy.mylibrary;
+
+/**
+ * zhangy
+ * Created by 1 on 2018/5/17.
+ */
+
+public class Test {
+
+    public double addition(double addnum1,double addnum2){
+        return addnum1+addnum2;
+    }
+}
